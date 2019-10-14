95511e5dcd80110a9321e0da4626a058197e2c33
Andriy Knysh <aknysh@users.noreply.github.com>
2019-10-13 20:29:08 -0400

    Update `atlantisVersion` (#33)

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ import (
 	"github.com/spf13/viper"
 )
 
-const atlantisVersion = "0.9.0-cp.2+2019-10-13T21:00:00+00:00"
+const atlantisVersion = "0.9.0.2+2019-10-14T12:25:00+00:00"
 
 func main() {
 	v := viper.New()
